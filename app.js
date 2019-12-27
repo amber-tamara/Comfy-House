a62fd5ffed6f473c983d15c2bb68cf0287a2290b
Amber Tamara <ambertjansen@gmail.com>
2019-12-27 20:57:52 +0000

    (getBagButtons)

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
-const productsDOM = document.querySelector(".product-center");
+const productsDOM = document.querySelector(".products-center");
 // cart
 let cart = [];
 
@@ -55,17 +55,43 @@ class UI {
         </article> `;
     });
     productsDOM.innerHTML = result;
-    console.log(products);
+  }
+  getBagButtuns() {
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+      if (inCart) {
+        button.innerText = "In Cart";
+        button.disabled = true;
+      } else {
+        button.addEventListener("click", event => {
+          console.log(event);
+        });
+      }
+    });
   }
 }
 
 //local storage
-class Storage {}
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
 
   //get all products
-  products.getProducts().then(products => ui.displayProducts(products));
+  products
+    .getProducts()
+    .then(products => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getBagButtuns();
+    });
 });
